72634f5b8b79012c05de678d0e4e60d9a4e339b3
Helena <hmmsjd@gmail.com>
2014-10-27 09:49:53 +0100

    Fet l'exercici de l'activitat 1.3.1
    Fet l'exercici de l'activitat 1.3.1

---
+++ Activitat 1.3.1/scope.js
@@ -0,0 +1,15 @@
+var elMeuContinentEs = "jo sóc d’una ciutat d’Europa";
+
+function ciutadaBCN() {
+	var joSocDe = "bcn";
+
+	document.getElementById("ciutats").innerHTML = " " + elMeuContinentEs;
+}
+
+
+function ciutadaPAR() {
+	var joSocDe = "par";
+
+	document.getElementById("ciutats").innerHTML = " " + elMeuContinentEs;
+}
+
