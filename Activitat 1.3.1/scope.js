22c55e38d1e81719c42bb02bbe5677c788510a60
Helena <hmmsjd@gmail.com>
2014-10-27 10:28:57 +0100

    Canvi en el document .js
    Canvi en el document .js

--- Activitat 1.3.1/scope.js
+++ Activitat 1.3.1/scope.js
@@ -13,3 +13,36 @@ function ciutadaPAR() {
 	document.getElementById("ciutats").innerHTML = " " + elMeuContinentEs;
 }
 
+
+var elMeuContinentEs = "Europa";
+
+function ciutadaBCN() {
+	var joSocDe = "bcn";
+
+	document.getElementById("ciutats").innerHTML = joSocDe + " " + elMeuContinentEs;
+}
+
+
+function ciutadaPAR() {
+	var joSocDe = "par";
+
+	document.getElementById("ciutats").innerHTML = joSocDe + " " + elMeuContinentEs;
+}
+
+
+
+var elMeuContinentEs = "Europa";
+var joSocDe = "Jo soc d'una ciutat d'"
+
+function ciutadaBCN() {
+	
+
+	document.getElementById("ciutats").innerHTML = joSocDe + elMeuContinentEs;
+}
+
+
+function ciutadaPAR() {
+
+
+	document.getElementById("ciutats").innerHTML = joSocDe + elMeuContinentEs;
+}
